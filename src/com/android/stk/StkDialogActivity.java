99972b891911487db04fbeeabbdfeb0895c8878e
Shriram Ganesh <sganesh@codeaurora.org>
2012-01-11 16:35:34 -0800

    Stk: Few STK fixes
    1) Fix for GCF T.C Launch Browser, connect to default URL
    
    GCF TC : 27.22.4.26.1.4.2, Seq 1.1, When launch browser
    proactive command is issued with default URL , browser
    should be launched with the default home page that it
    is set to and not a harcoded home page.
    
    2) StkDialog activity should not cancel timer when it is paused
    
    During display text,  if any higher / lower priority events
    such as incoming call, new sms, screen off intent,
    notification alerts, user actions such as 'User moving to
    another activtiy' etc.. occur during Display Text ongoing session,
    this activity would receive 'onPause()' evt resulting in
    cancellation of the timer. As a result no terminal response is
    sent to the card
    
    3) StkMenu/StkInput activity should not cancel timer when paused
    
    During SelectItem / GetInkey / GetInput, if any
    higher / lower priority events such as incoming call,
    new sms, screen off intent,notification alerts,
    user actions such as 'User moving to another activtiy' etc..
    occur during SELECT ITEM / GETINKEY / GETINPUT ongoing session
    respectively, the respective activity would receive
    'onPause()' evt resulting in cancellation of the timer.
    As a result no terminal response is sent to the card
    
    4) Fix OPEN CHANNEL user confirmation
    
    On receipt of OPEN CHANNEL , user confirmation needs to be set
    if user acknowledges the message / command.
    
    5) Show 'Toast indications' SEND DATA / RECV DATA and CLOSE CHANNEL
    
    Displaying Toast indication yields better UI experience when there are
    multiple SEND / RCV DATA proactive commands in a single OPEN CHANNEL
    session.
    
    Change-Id: I83ebaa33fa1e35781022cc04d57c5a5bd2a24f9d

--- src/com/android/stk/StkDialogActivity.java
+++ src/com/android/stk/StkDialogActivity.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2007 The Android Open Source Project
+ * Copyright (c) 2009,2012, The Linux Foundation. All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -133,7 +133,15 @@ public class StkDialogActivity extends Activity implements View.OnClickListener 
     public void onPause() {
         super.onPause();
 
-        cancelTimeOut();
+        /*
+         * do not cancel the timer here cancelTimeOut(). If any higher/lower
+         * priority events such as incoming call, new sms, screen off intent,
+         * notification alerts, user actions such as 'User moving to another activtiy'
+         * etc.. occur during Display Text ongoing session,
+         * this activity would receive 'onPause()' event resulting in
+         * cancellation of the timer. As a result no terminal response is
+         * sent to the card.
+         */
     }
 
     @Override
