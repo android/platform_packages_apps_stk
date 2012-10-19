73081bb4f54727c437fe6ea362fc8fe29cb1d7dd
Suresh Koleti <skolet@codeaurora.org>
2012-10-19 01:17:24 -0700

    Stk: STK Call Control feature implementation
    Change-Id: Ib3135f6c6120ffaafcb8898e2722d345435da69c

--- src/com/android/stk/StkAppService.java
+++ src/com/android/stk/StkAppService.java
@@ -103,6 +103,8 @@ public class StkAppService extends Service implements Runnable {
     static final int OP_END_SESSION = 4;
     static final int OP_BOOT_COMPLETED = 5;
     private static final int OP_DELAYED_MSG = 6;
+    
+    static final int OP_ALPHA_NOTIFY = 30;
 
     // Response ids
     static final int RES_ID_MENU_SELECTION = 11;
@@ -181,6 +183,7 @@ public class StkAppService extends Service implements Runnable {
             msg.obj = args.getParcelable(CMD_MSG);
             break;
         case OP_RESPONSE:
+        case OP_ALPHA_NOTIFY:
             msg.obj = args;
             /* falls through */
         case OP_LAUNCH_APP:
@@ -310,6 +313,9 @@ public class StkAppService extends Service implements Runnable {
                 break;
             case OP_DELAYED_MSG:
                 handleDelayedCmd();
+                break;
+            case OP_ALPHA_NOTIFY:
+                handleAlphaNotify((Bundle) msg.obj);
                 break;
             }
         }
@@ -913,5 +919,14 @@ public class StkAppService extends Service implements Runnable {
             return true;
         }
         return false;
+    }
+
+    private void handleAlphaNotify(Bundle args) {
+        String alphaString = args.getString(AppInterface.ALPHA_STRING);
+
+        CatLog.d(this, "Alpha string received from card: " + alphaString);
+        Toast toast = Toast.makeText(this, alphaString, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.TOP, 0, 0);
+        toast.show();
     }
 }
