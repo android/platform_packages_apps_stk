aefee66ba6afc8c688312f7c99065256f69519aa
Sandeep Kunta <skunta@codeaurora.org>
2013-06-17 22:09:37 +0530

    MSIM: Add support for MSIM STK
    Extended base stk functionality to support stk for
    3 different cards. User will see 3 stk icons in launcher screen
    if 3 cards inserted which support stk
    
    Change-Id: Icf5997dd6a6ba5e1de675cf5f4334c78c2c037f1

--- src/com/android/stk/ToneDialog.java
+++ src/com/android/stk/ToneDialog.java
@@ -39,6 +39,7 @@ public class ToneDialog extends Activity {
     ToneSettings settings = null;
     TonePlayer player = null;
     boolean mIsResponseSent = false;
+    private int mSlotId = 0;
 
     /**
      * Handler used to stop tones from playing when the duration ends.
@@ -126,12 +127,14 @@ public class ToneDialog extends Activity {
         }
         toneMsg = intent.getParcelableExtra("TEXT");
         settings = intent.getParcelableExtra("TONE");
+        mSlotId = intent.getIntExtra(StkAppService.SLOT_ID, 0);
     }
 
     private void sendResponse(int resId) {
         Bundle args = new Bundle();
         args.putInt(StkAppService.OPCODE, StkAppService.OP_RESPONSE);
         args.putInt(StkAppService.RES_ID, resId);
+        args.putInt(StkAppService.SLOT_ID, mSlotId);
         startService(new Intent(this, StkAppService.class).putExtras(args));
         mIsResponseSent = true;
     }
