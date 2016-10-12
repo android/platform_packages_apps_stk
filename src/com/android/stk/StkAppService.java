3b361a76ce071be803bc885cac53a4264150308b
Ryuto Sawada <ryuto.x.sawada@sonymobile.com>
2016-10-12 17:16:56 +0900

    Empty toast should not be displayed
    Empty toast is displayed if SIM card does not specify any alpha
    identifier in some proactive commands like SEND SMS. If msg.txt is null,
    it should not be displayed.
    
    Bug: 32103285
    Test: Confirmed that no toast is created when msg.txt is null.
    
    Change-Id: I4daa512a39da8fc79656511a346d180957f555b2

--- src/com/android/stk/StkAppService.java
+++ src/com/android/stk/StkAppService.java
@@ -1460,7 +1460,7 @@ public class StkAppService extends Service implements Runnable {
     }
 
     private void launchEventMessage(int slotId, TextMessage msg) {
-        if (msg == null || (msg.text != null && msg.text.length() == 0)) {
+        if (msg == null || TextUtils.isEmpty(msg.text)) {
             CatLog.d(LOG_TAG, "launchEventMessage return");
             return;
         }
