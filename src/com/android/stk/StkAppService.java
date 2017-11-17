9e5eed9ef2ad822b96252a8cd79e7421a6ea7e17
Yoshiaki Naka <yoshiaki.naka@sony.com>
2017-11-17 21:51:55 +0900

    Utilize dynamic broadcast receiver for ACTION_LOCALE_CHANGED broadcast
    Intent.ACTION_LOCALE_CHANGED broadcast is received by the static
    broadcast receiver in order to generate LANGUAGE SELECTION event, but it
    should be better to use a dynamic broadcast receiver for it only when
    LANGUAGE SELECTION event is requested by the card.
    
    Bug: 69524582
    Test: Confirmed that LANGUAGE SELECTION event can be made as before.
    
    Change-Id: I4ce0b6378bb3024441012ced7a5c0cfb2f2a0760

--- src/com/android/stk/StkAppService.java
+++ src/com/android/stk/StkAppService.java
@@ -27,6 +27,7 @@ import android.app.Service;
 import android.app.Activity;
 import android.app.ActivityManagerNative;
 import android.app.IProcessObserver;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -164,6 +165,7 @@ public class StkAppService extends Service implements Runnable {
     private StkContext[] mStkContext = null;
     private int mSimCount = 0;
     private IProcessObserver.Stub mProcessObserver = null;
+    private BroadcastReceiver mLocaleChangeReceiver = null;
     private TonePlayer mTonePlayer = null;
     private Vibrator mVibrator = null;
 
@@ -377,6 +379,7 @@ public class StkAppService extends Service implements Runnable {
     public void onDestroy() {
         CatLog.d(LOG_TAG, "onDestroy()");
         unregisterProcessObserver();
+        unregisterLocaleChangeReceiver();
         sInstance = null;
         waitForLooper();
         mServiceLooper.quit();
@@ -1489,6 +1492,8 @@ public class StkAppService extends Service implements Runnable {
                 unregisterProcessObserver(AppInterface.CommandType.SET_UP_EVENT_LIST, slotId);
                 break;
             case LANGUAGE_SELECTION_EVENT:
+                unregisterLocaleChangeReceiver();
+                break;
             default:
                 break;
         }
@@ -1504,6 +1509,8 @@ public class StkAppService extends Service implements Runnable {
                     registerProcessObserver();
                     break;
                 case LANGUAGE_SELECTION_EVENT:
+                    registerLocaleChangeReceiver();
+                    break;
                 default:
                     break;
             }
@@ -1570,6 +1577,28 @@ public class StkAppService extends Service implements Runnable {
             } catch (RemoteException e) {
                 CatLog.d(this, "Failed to unregister the process observer");
             }
+        }
+    }
+
+    private void registerLocaleChangeReceiver() {
+        if (mLocaleChangeReceiver == null) {
+            mLocaleChangeReceiver = new BroadcastReceiver() {
+                @Override public void onReceive(Context context, Intent intent) {
+                    if (Intent.ACTION_LOCALE_CHANGED.equals(intent.getAction())) {
+                        Message message = mServiceHandler.obtainMessage();
+                        message.arg1 = OP_LOCALE_CHANGED;
+                        mServiceHandler.sendMessage(message);
+                    }
+                }
+            };
+            registerReceiver(mLocaleChangeReceiver, new IntentFilter(Intent.ACTION_LOCALE_CHANGED));
+        }
+    }
+
+    private void unregisterLocaleChangeReceiver() {
+        if (mLocaleChangeReceiver != null) {
+            unregisterReceiver(mLocaleChangeReceiver);
+            mLocaleChangeReceiver = null;
         }
     }
 
