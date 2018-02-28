5cdc8a69b0d64d923427e12d6bb8f83ff9b93ed2
Jesse Fuentes <jessef@quicinc.com>
2018-03-01 02:20:28 +0530

    Add large text view for 'SET UP IDLE MODE TEXT'
    With the current functionality the GCF test case
    'GCF TC 27.22.4.22.1.7- SETUP IDLE MODE Text- large
    text string' the text is not completely seen by the
    user.
    
    Add big view notification style for displaying the text
    string for 'SET UP IDLE MODE TEXT'.
    
    Handle UNSOL_STK_EVENT_NOTIFY command message properly.
    
    Currently, in scenario where the UNSOL_STK_EVENT_NOTIFY is
    handled, and there is a main menu selection followed by,results
    in sending of unwanted terminal response instead of envelope
    command from the framework.
    
    Set the CurrentCmd to MainCmd after launching the event
    message in StkAppService, while handling UNSOL_STK_EVENT_NOTIFY.
    This ensures the CurrentCmd still points to SEND_SMS instead of
    SET_UP_MENU.
    
    Change-Id: Ia8ae7eaecd5f18bc2b3565655acee063b0230cb2

--- src/com/android/stk/StkAppService.java
+++ src/com/android/stk/StkAppService.java
@@ -1125,6 +1125,10 @@ public class StkAppService extends Service implements Runnable {
         case GET_CHANNEL_STATUS:
             waitForUsersResponse = false;
             launchEventMessage(slotId);
+            //Reset the mCurrentCmd to mMainCmd, to avoid wrong TR sent for
+            //SEND_SMS/SS/USSD, when user launches STK app next time and do
+            //a menu selection.
+            mStkContext[slotId].mCurrentCmd = mStkContext[slotId].mMainCmd;
             break;
         case REFRESH:
             waitForUsersResponse = false;
@@ -2149,6 +2153,8 @@ public class StkAppService extends Service implements Runnable {
                     .setSmallIcon(com.android.internal.R.drawable.stat_notify_sim_toolkit);
             notificationBuilder.setContentIntent(pendingIntent);
             notificationBuilder.setOngoing(true);
+            notificationBuilder.setStyle(new Notification.BigTextStyle(notificationBuilder)
+                    .bigText(msg.text));
             notificationBuilder.setOnlyAlertOnce(true);
             // Set text and icon for the status bar and notification body.
             if (mStkContext[slotId].mIdleModeTextCmd.hasIconLoadFailed() ||
