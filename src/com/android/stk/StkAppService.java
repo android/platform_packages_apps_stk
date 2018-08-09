165a9076bd6a75633311349b54d2c61274d9c257
Yoshiaki Naka <yoshiaki.naka@sony.com>
2018-08-09 18:41:31 +0900

    Fix dissymmetric pending activity handling and state management
    StkMenuActivity for secondary menu and StkInputActivity currently
    register theirselves as pending activity, but it is never unregistered
    by theirselves. Pending activity should be unregistered when it is
    brought back to the foreground unless terminal response has already been
    sent to the card.
    
    Change-Id: I335d1867c121191b31d8436eea4982a7d45a1c90

--- src/com/android/stk/StkAppService.java
+++ src/com/android/stk/StkAppService.java
@@ -635,11 +635,12 @@ public class StkAppService extends Service implements Runnable {
             case OP_SET_ACT_INST:
                 Activity act = (Activity) msg.obj;
                 if (mStkContext[slotId].mActivityInstance != act) {
-                    CatLog.d(LOG_TAG, "Set activity instance - " + act);
+                    CatLog.d(LOG_TAG, "Set pending activity instance - " + act);
                     Activity previous = mStkContext[slotId].mActivityInstance;
                     mStkContext[slotId].mActivityInstance = act;
                     // Finish the previous one if it has not been finished yet somehow.
-                    if (previous != null && !previous.isDestroyed() && !previous.isFinishing()) {
+                    if (act != null && previous != null && !previous.isDestroyed()
+                            && !previous.isFinishing()) {
                         CatLog.d(LOG_TAG, "Finish the previous pending activity - " + previous);
                         previous.finish();
                     }
